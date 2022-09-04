74ca867c5d00a846eace4fd5c35cf7e56490d81c
silasbue <48789220+silasbue@users.noreply.github.com>
2022-09-04 12:57:31 +0200

    Implemented error handling (#3)

--- LeapYear/LeapYearChecker.cs
+++ LeapYear/LeapYearChecker.cs
@@ -4,6 +4,10 @@ public class LeapYearChecker
 {
   public bool isLeapYear(int year)
   {
+    if (year < 1582)
+    {
+      throw new ArgumentException("year has to be greater than or equal to 1582");
+    }
     return (!(year % 100 == 0 && !(year % 400 == 0)) && year % 4 == 0);
   }
 }
--- LeapYear/Program.cs
+++ LeapYear/Program.cs
@@ -1,14 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 var leapYearChecker = new LeapYear.LeapYearChecker();
-Console.WriteLine("enter year: ");
-Console.Out.Flush();
-int input = int.Parse(Console.ReadLine()!);
 
-if (leapYearChecker.isLeapYear(input))
+Console.WriteLine("enter year: ");
+var input = Console.ReadLine();
+
+if (input is null)
 {
-  Console.WriteLine("yay");
+  Console.WriteLine("please provide a number");
+  return;
 }
-else
+
+try
 {
-  Console.WriteLine("nay");
+  int number = int.Parse(input);
+  if (leapYearChecker.isLeapYear(number))
+  {
+    Console.WriteLine("yay");
+  }
+  else
+  {
+    Console.WriteLine("nay");
+  }
+}
+catch (System.FormatException)
+{
+  Console.WriteLine("please provide a number");
+}
+catch (System.ArgumentException e)
+{
+  Console.WriteLine(e.Message);
 }
